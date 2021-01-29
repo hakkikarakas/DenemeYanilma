08b8799e391eabfcbee8f2c372a0253c78505a10
hakkı karakaş <hakkikarks@gmail.com>
2021-01-30 02:49:50 +0300

    Add project files.

---
+++ DenemeYanılma/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        LastikTuru araba = new LastikTuru();
+        araba.Coupe = "BMW";
+        
+        YakitTuru benzin = new YakitTuru();
+        benzin.Coupe = "Vw";
+        ArabaTuru araba2 = araba;
+
+
+        Galeri galeri = new Galeri();
+        //galeri.Satis(benzin);
+        galeri.Satis(araba2);
+       // galeri.Satis(araba);  
+
+    }
+}
+//base class:ArabaTuru
+class ArabaTuru
+{
+    public string Sedan { get; set; }
+    public string Coupe { get; set; }
+    public string Suv { get; set; }
+}
+class LastikTuru:ArabaTuru
+{
+    public string Kis { get; set; }
+    public string Yaz { get; set; }
+}
+class YakitTuru:ArabaTuru
+{
+    public string Dizel { get; set; }
+    public string Benzin { get; set; }
+}
+
+class Galeri
+{
+    public void Satis(ArabaTuru arabaTuru)  //Base class içerdiği için her iki sınıf için çalıştı.
+    {                                       //Aynı kodu farklı nesnelerde çalıştırabildik.                            
+        Console.WriteLine(arabaTuru.Coupe + "  Satış gerçekleşti.");
+    }
+}
