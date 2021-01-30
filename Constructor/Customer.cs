f854b0ce87c589b2a33cab06149afe9044219876
hakkı karakaş <hakkikarks@gmail.com>
2021-01-30 18:49:51 +0300

    ++

---
+++ Constructor/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    class Customer
+    {
+        //default constructor
+        public Customer()
+        {
+
+        }
+        public Customer(int Id, string Firstname, string Lastname, string City)
+        {
+            this.Id = Id;
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.City = City;
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string City { get; set; }
+
+    }
+
+}
--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -25,24 +25,3 @@ namespace Constructor
         }
     }
 }
-class Customer
-{
-    //default constructor
-    public Customer()
-    {
-
-    }
-    public Customer(int Id,string Firstname,string Lastname,string City)
-    {
-        this.Id = Id;
-        this.Firstname =Firstname;
-        this.Lastname = Lastname;
-        this.City = City;
-    }
-
-    public int Id { get; set; }
-    public string Firstname { get; set; }
-    public string Lastname { get; set; }
-    public string City { get; set; }
-
-}
