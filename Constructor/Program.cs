ce3fede3c503571e0a33cf83f1bffca05e6835d8
hakkı karakaş <hakkikarks@gmail.com>
2021-01-30 18:48:06 +0300

    DefaultConstructor

---
+++ Constructor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer() { Id = 1, Firstname = "Hakkı", Lastname = "Karakaş", City = "İzmir" };
+            Customer customer1 = new Customer();
+            customer1.Id = 2;
+            customer1.Firstname = "Ali";
+            customer1.Lastname = "Veli";
+            customer1.City = "İstanbul";
+            //customer ve customer1 yazım şekilleri aynıdırlar.
+
+            Customer customer2 = new Customer(3,"Veli","Ali","Erzurum");
+            List<Customer> customers = new List<Customer>() { customer,customer1,customer2};
+            foreach (var item in customers)
+            {
+                Console.WriteLine(item.Id+" "+ item.Firstname + " " + item.Lastname + " " + item.City );
+            }
+            
+        }
+    }
+}
+class Customer
+{
+    //default constructor
+    public Customer()
+    {
+
+    }
+    public Customer(int Id,string Firstname,string Lastname,string City)
+    {
+        this.Id = Id;
+        this.Firstname =Firstname;
+        this.Lastname = Lastname;
+        this.City = City;
+    }
+
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string City { get; set; }
+
+}
