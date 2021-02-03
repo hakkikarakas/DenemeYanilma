a4799b67e5265e2d91919c59041a9cf6382e8522
hakkı karakaş <hakkikarks@gmail.com>
2021-02-03 19:21:58 +0300

    Tekrar

---
+++ Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[]
+            {
+                new Customer{FirstName="Hakkı"},
+                new Student{ FirstName="Ali"},
+                new Person{FirstName="Veli"}
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+    class Student:Person
+    {
+        public string Department { get; set; }
+    }
+}
---
+++ Intefaces/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal []
+            {
+                new SqlServerCustomerDal(),
+                new OracleServerCustomerDal()
+            };
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();//Hem oracle hem sql eklendi.
+            }
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Hakkı",
+                LastName = "Karakaş",
+                Adress = "İzmir"
+            };
+            Student student = new Student
+            {
+
+                Id = 1,
+                FirstName = "Hakkı1",
+                LastName = "Karakaş1",
+                Department = "Matematik"
+            };
+            manager.Add(customer);
+            manager.Add(student);
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+    }
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
