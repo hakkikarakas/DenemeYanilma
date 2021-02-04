167ec838c1befdd7637eb8be18e945fb0aefef93
hakkı karakaş <hakkikarks@gmail.com>
2021-02-04 19:23:01 +0300

    Test

---
+++ Interfaces2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Interfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPersonManager customerManager = new CustomerManager();
+            
+            //IPersonManager employeeManager = new EmployeeManager();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new CustomerManager());
+          
+        }
+    }
+    interface IPersonManager 
+    {
+        void Add();
+        void Update();
+    }
+
+    //inherits-Class-------implements-interface 
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi.");
+        }
+    }
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi.");
+        }
+    }
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+}
